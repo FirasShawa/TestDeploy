d7f5963145f39ade3ca919b050c4a5d11aaa9eff
Firas Shawa <Frias.shawa22@gmail.com>
2019-06-19 02:59:16 +0300

    setup files

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express")
+
+const app = express()
+
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const path = require("path")
+
+const port = process.env.PORT || 4000 
+
+app.use(cors(),bodyParser.json())
+
+app.get("/myapi/Firas",(req,res) => {
+    res.send("hi Firas");
+})
+
+app.post("/myapi/Name",(req,res)=>{
+    // let username = req.body.name;
+    res.send("Hi " + req.body.name)
+})
+
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"client/bulid")))
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"client/build","index.html"))
+    })
+}
+
+
+
+app.listen(port,()=>{
+    console.log("Server is running on port 4000")
+})
